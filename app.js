f4a537cee46f4a8ea591bd5d3a3a3cf3c53f2f55
Erin Hinson <erin.hinson@xcelc02q33v.attlocal.net>
2016-12-09 18:32:44 -0500

    Play multiple keys

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var keys = document.querySelectorAll('.key');
 var sounds = document.getElementById('sounds');
 var audio = '';
-
-window.addEventListener('keydown', function () {
-  for (let i = 0; i < keys.length; i++) {
-    var attr = keys[i].dataset.key;
-    audio = sounds.querySelector(`audio[data-key='65']`);
+var attr = '';
+var pressed = '';
 
 
+window.addEventListener('keydown', function (e) {
+  if (e.shiftKey && e.keyCode){
+    console.log(e.keyCode);
   }
+  else {
+    console.log(e.keyCode);
+  }
+  audio = sounds.querySelector(`audio[data-key='${e.keyCode}']`);
+
   audio.play();
+  audio = '';
 });
+
+console.log(audio);
