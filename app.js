972cf3ccf9c63795e8c6a57b5d646746a0f819ff
Erin Hinson <hinson.erin@gmail.com>
2016-12-09 20:48:55 -0500

    Coordinate keypress css and sound

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var keys = document.querySelectorAll('.key');
 var sounds = document.getElementById('sounds');
 
-
-
 window.addEventListener('keydown', function (e) {
   var attr = e.keyCode;
   var audio = document.querySelector(`audio[data-key="${attr}"]`);
@@ -14,11 +12,17 @@ window.addEventListener('keydown', function (e) {
   else{
     console.log(audio);
     key.classList.add('playing');
+    audio.currentTime = 0;
+
     audio.play();
+    audio.loop = false;
 
   }
 });
 
 window.addEventListener('keyup', function (e) {
+  var attr = e.keyCode;
+  var key = document.querySelector(`div[data-key="${attr}"]`);
+  key.classList.remove('playing');
 
 });
